5b258a0c0d3fc4ff11fcd97efbd35fce8af37e88
Luca <latrombetta@icloud.com>
2021-09-20 11:11:49 -0400

    update

--- links.py
+++ links.py
@@ -1,12 +1,12 @@
 links = {
-    'lower-squatrack': '',
-    'lower-dumbbell': '',
-    'upper-squatrack': '',
-    'upper-dumbbell': '',
-    'cardio-bike': '',
-    'cardio-stairs': '',
-    'cardio-treadmill': '',
-    'stacked-weights': ''
+    'lower-squatrack': 'https://getactive.gogaelsgo.com/Program/GetProgramDetails?courseId=bacb7416-d62f-4792-a18d-2256faf00375&semesterId=01ceaf62-f800-498f-85aa-8930a361b2b5',
+    'lower-dumbbell': 'https://getactive.gogaelsgo.com/Program/GetProgramDetails?courseId=af6a4272-ae80-43b1-b43b-59c443790277&semesterId=01ceaf62-f800-498f-85aa-8930a361b2b5',
+    'upper-squatrack': 'https://getactive.gogaelsgo.com/Program/GetProgramDetails?courseId=b80d2829-2344-44fc-8f6d-266da722e7f6&semesterId=01ceaf62-f800-498f-85aa-8930a361b2b5',
+    'upper-dumbbell': 'https://getactive.gogaelsgo.com/Program/GetProgramDetails?courseId=1125d7e6-c0f9-4dbd-b490-ebb717a0f104&semesterId=01ceaf62-f800-498f-85aa-8930a361b2b5',
+    'cardio-bike': 'https://getactive.gogaelsgo.com/Program/GetProgramDetails?courseId=46aa3a1c-4ab5-4e47-b9db-e60c7450637b&semesterId=01ceaf62-f800-498f-85aa-8930a361b2b5',
+    'cardio-stairs': 'https://getactive.gogaelsgo.com/Program/GetProgramDetails?courseId=2fd2c803-6110-4f0c-9238-11097f6e5f03&semesterId=01ceaf62-f800-498f-85aa-8930a361b2b5',
+    'cardio-treadmill': 'https://getactive.gogaelsgo.com/Program/GetProgramDetails?courseId=8c191d1e-1522-4648-af29-1122234cc120&semesterId=01ceaf62-f800-498f-85aa-8930a361b2b5',
+    'stacked-weights': 'https://getactive.gogaelsgo.com/Program/GetProgramDetails?courseId=fc645305-13fa-4bf3-8bab-51bd6779e13e&semesterId=01ceaf62-f800-498f-85aa-8930a361b2b5'
 }
 
 zone_time_data = {
